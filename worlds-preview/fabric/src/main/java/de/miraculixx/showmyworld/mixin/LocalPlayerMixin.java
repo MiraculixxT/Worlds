89f14967225fac1fb9498916dac072e5419197b4
miraculixx <miraculixxyt@gmail.com>
2026-09-17 15:18:08 +0200

    ShowMyWorld: Fixing panoramas bug out on mounts (vanilla bug)

---
+++ worlds-preview/fabric/src/main/java/de/miraculixx/showmyworld/mixin/LocalPlayerMixin.java
@@ -0,0 +1,24 @@
+package de.miraculixx.showmyworld.mixin;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/**
+ * A passengers view yaw is its head rotation, which {@code grabPanoramixScreenshot} never turns,
+ * so all four side faces came out identical while mounted (vanilla bug)
+ */
+@Mixin(LocalPlayer.class)
+public abstract class LocalPlayerMixin {
+
+    @Inject(method = "getViewYRot", at = @At("HEAD"), cancellable = true)
+    private void showmyworld$panoramaYaw(float a, CallbackInfoReturnable<Float> cir) {
+        LocalPlayer self = (LocalPlayer) (Object) this;
+        if (self.isPassenger() && Minecraft.getInstance().gameRenderer.mainCamera().isPanoramicMode()) {
+            cir.setReturnValue(self.getYRot());
+        }
+    }
+}
